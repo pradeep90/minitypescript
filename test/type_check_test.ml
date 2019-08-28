a8f2e57c31ca97084f8b24bac21565b42a9b431d
S Pradeep Kumar <gohanpra@gmail.com>
2019-08-29 04:15:47 +0530

    typecheck: add helper for asserting type errors.

--- test/type_check_test.ml
+++ test/type_check_test.ml
@@ -2,6 +2,15 @@ open OUnit
 open Type_check
 open Syntax
 
+let assert_type_error fn error_msg =
+  try
+    fn();
+    failwith "expected type error"
+  with
+  | Type_error msg -> if msg = error_msg then ()
+                      else failwith ("wrong type error; expected "
+                                     ^ error_msg ^ " but got " ^ msg)
+;;
 
 (* Test Fixture *)
 let test_fixture = "type_check" >:::
