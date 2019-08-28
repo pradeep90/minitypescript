a7ea736e6293b23602c80a2325d4676bd56f9f6e
S Pradeep Kumar <gohanpra@gmail.com>
2019-08-29 02:09:44 +0530

    eval: test Var.

--- test/eval_test.ml
+++ test/eval_test.ml
@@ -1,5 +1,6 @@
 open OUnit
 open Eval
+open Syntax
 
 
 (* Test Fixture *)
@@ -7,6 +8,10 @@ let test_fixture = "eval" >:::
 [
   "lookup_value" >:: ( fun () ->
     assert_equal 3 (lookup_value "boyz" [("yo",4); ("boyz",3)]) ~printer:string_of_int;
+  );
+
+  "Var" >:: (fun () ->
+    assert_equal (Var "boyz") (eval [("yo", Var "boyz")] (Var "yo"))
   );
 ]
 
